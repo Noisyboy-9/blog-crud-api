1e62d94c2c5e5563ecaf7b485f6aa3533f128109
sina shariati <sina.shariati@yahoo.com>
2020-09-29 16:42:55 +0330

    refactored PostFeatureTest to use makePost and createPost helper functions

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function store(Request $request)
     {
-        $attributes = $request->validate([
+        $attributes = $this->validate($request, [
             'title' => 'required|unique:posts',
             'body' => 'required'
         ]);
 
-        Post::create($attributes);
+        if (Post::create($attributes)) {
+            return response()->json([
+                'created' => true,
+                'data' => $attributes
+            ], 201);
+        }
     }
 }
--- tests/PostFeatureTest.php
+++ tests/PostFeatureTest.php
@@ -1,24 +1,75 @@
 <?php
 
 
+use App\Models\Post;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class PostFeatureTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * create a post and persist it to database
+     *
+     * @param array $attributes
+     *
+     * @note this function persists data to database
+     * @return array
+     */
+    private function createPost(array $attributes = [])
+    {
+        $post = Post::factory()->make($attributes);
+        $post->save();
+
+        return $post->toArray();
+    }
+
+    /**
+     * make a post array but don't persist it in database
+     *
+     * @param array $attributes
+     *
+     * @note this function does not persist data to database
+     * @return array
+     */
+    private function makePost(array $attributes = [])
+    {
+        return Post::factory()->make($attributes)->toArray();
+    }
+
+
     /** @test * */
     public function a_post_can_be_created()
     {
-        $this->withoutExceptionHandling();
+        $post = $this->makePost();
 
-        $post = [
-            'title' => 'post title',
-            'body' => 'post body'
-        ];
-
-        $this->post('/posts', $post)->assertResponseStatus(201);
+        $this->post('/posts', $post)
+            ->seeJson(['created' => true])
+            ->seeJson(['data' => $post])
+            ->assertResponseStatus(201);
 
         $this->seeInDatabase('posts', $post);
     }
+
+    /** @test * */
+    public function a_post_must_have_a_title()
+    {
+        $post = $this->makePost(['title' => null]);
+
+        $this->post('/posts', $post)->assertResponseStatus(422);
+
+
+        $this->notSeeInDatabase('posts', $post);
+    }
+
+    /** @test * */
+    public function a_post_must_have_a_body()
+    {
+        $post = $this->makePost(['body' => null]);
+
+        $this->post('/posts', $post)->assertResponseStatus(422);
+
+        $this->notSeeInDatabase('posts', $post);
+    }
+
 }
