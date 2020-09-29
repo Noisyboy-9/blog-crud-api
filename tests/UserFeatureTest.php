604bccac3021271372acc5284f747fa03914d6ee
sina shariati <sina.shariati@yahoo.com>
2020-09-29 21:43:03 +0330

    added feature to hash passwords when saving in database

--- tests/UserFeatureTest.php
+++ tests/UserFeatureTest.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class UserFeatureTest extends TestCase
@@ -26,5 +28,27 @@ class UserFeatureTest extends TestCase
             'username' => 'noisyboy',
             'email' => 'sina.shariati@yahoo.com',
         ]);
+    }
+
+    /** @test * */
+    public function a_created_user_has_hashed_password()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = [
+            'username' => 'noisyboy',
+            'email' => 'sina.shariati@yahoo.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $this->post('/register', $user)
+            ->assertResponseStatus(201);
+
+
+        $encryptedPassword = DB::table('users')->first()->password;
+        $decryptedPassword = Crypt::decrypt($encryptedPassword);
+
+        $this->assertEquals($decryptedPassword, $user['password']);
     }
 }
